0c3e4b5326923724329d274a3335a559898b548d
stevesdm <stevejobson@163.com>
2017-12-26 17:32:01 +0800

    transient

---
+++ Improve/src/com/steve/transientdemo/Test.java
@@ -0,0 +1,61 @@
+package com.steve.transientdemo;
+
+import java.io.*;
+import java.util.Date;
+
+/**
+ * @Description:
+ * @Author: stevejobson
+ * @CreateDate: 2017/12/26 下午5:18
+ */
+public class Test {
+
+
+    //正常序列化反序列化
+    @org.junit.Test
+    public void test1() throws IOException, ClassNotFoundException {
+        User1 user1 = new User1();
+        user1.setId(1);
+        user1.setUserName("steve");
+        user1.setDate(new Date());
+
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(
+                new File("0xjh000")));
+        out.writeObject(user1);
+        out.close();
+
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(
+                new File("0xjh000")));
+        User1 testUser = (User1) in.readObject();
+        in.close();
+
+        System.out.println(testUser.getId());
+        System.out.println(testUser.getUserName());
+        System.out.println(testUser.getDate());
+
+    }
+
+    //transient修饰的不进行序列化
+    @org.junit.Test
+    public void test2() throws IOException, ClassNotFoundException {
+        User2 user2 = new User2();
+        user2.setId(1);
+        user2.setUserName("steve");
+        user2.setDate(new Date());
+
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(
+                new File("0xjh001")));
+        out.writeObject(user2);
+        out.close();
+
+        ObjectInputStream in = new ObjectInputStream(new FileInputStream(
+                new File("0xjh001")));
+        User2 testUser = (User2) in.readObject();
+        in.close();
+
+        System.out.println(testUser.getId());
+        System.out.println(testUser.getUserName());
+        System.out.println(testUser.getDate());
+
+    }
+}
---
+++ Improve/src/com/steve/transientdemo/User1.java
@@ -0,0 +1,42 @@
+package com.steve.transientdemo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @Description:
+ * @Author: stevejobson
+ * @CreateDate: 2017/12/26 下午5:09
+ */
+public class User1 implements Serializable {
+
+
+    private int id;
+    private String userName;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
