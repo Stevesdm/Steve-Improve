4b8e28579caff2b2a216bbefd092e97b90ac2f40
stevesdm <stevejobson@163.com>
2018-01-23 14:15:03 +0800

    add String and Integer

---
+++ Improve/src/com/steve/Integer/Demo.java
@@ -0,0 +1,24 @@
+package com.steve.Integer;
+
+/**
+ * @Description:
+ * @Author: stevejobson
+ * @CreateDate: 2018/1/22 下午6:32
+ */
+public class Demo {
+
+
+    public static void main(String[] args) {
+        Integer a1 = 126;
+        Integer a2 = 126;
+
+        System.out.println(a1 == a2);
+
+        Integer a3 = 129;
+        Integer a4 = 129;
+        System.out.println(a3 == a4);
+
+
+
+    }
+}
---
+++ Improve/src/com/steve/string/Demo.java
@@ -0,0 +1,17 @@
+package com.steve.string;
+
+/**
+ * @Description:
+ * @Author: stevejobson
+ * @CreateDate: 2018/1/22 下午6:45
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+
+        //阿里巴巴规范，判断最后一个分隔符前有木有元素
+        String str ="a,v,c,,";
+        String[] d = str.split(",");
+        System.out.println(d.length);
+    }
+}
